3a327562b95d71d6937ea64955843f1994a23dff
Matheesh <matheeshprabhu123@gmail.com>
2021-10-30 23:58:19 +0530

    Revert "Renaming Greeting interface name"
    This reverts commit 927cdd19bfe77b41e3a73b5ffac221d91b0d85fe.

--- src/main/java/com/math/springframework/controllers/PrimaryController.java
+++ src/main/java/com/math/springframework/controllers/PrimaryController.java
@@ -1,18 +1,18 @@
 package com.math.springframework.controllers;
 
-import com.math.springframework.services.GreetingService;
+import com.math.springframework.services.Greeting;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PrimaryController {
 
-    private final GreetingService greetingService;
+    private final Greeting greeting;
 
-    public PrimaryController(GreetingService greetingService) {
-        this.greetingService = greetingService;
+    public PrimaryController(Greeting greeting) {
+        this.greeting = greeting;
     }
 
     public String getGreeting(){
-        return greetingService.getGreeting();
+        return greeting.getGreeting();
     }
 }
